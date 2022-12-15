eccc0687fe3e69bdfb5dbbc7341f7fabb721b630
aquapaka <aqua.tamlong@gmail.com>
2022-12-15 19:08:21 +0700

    Add isDead property and dead animation

--- src/main/java/com/aquapaka/model/Boat.java
+++ src/main/java/com/aquapaka/model/Boat.java
@@ -2,10 +2,19 @@ package com.aquapaka.model;
 
 public class Boat extends Entity {
     private static final String imageUrl = "/image/Player_Boat.png";
-    private static final double JUMP_POWER = 6;
+    private static final double JUMP_POWER = 7;
+    private boolean isDead = false;
 
     public Boat(double positionX, double positionY, double velocityX, double velocityY, double accelerationX, double accelerationY) {
         super(imageUrl, 6, positionX, positionY, velocityX, velocityY, accelerationX, accelerationY);
+    }
+
+    public boolean isDead() {
+        return isDead;
+    }
+
+    public void setDead(boolean dead) {
+        isDead = dead;
     }
 
     @Override
@@ -13,7 +22,12 @@ public class Boat extends Entity {
         super.update();
 
         // Fix velocityY if player boat about to jump out of screen
-        if(getY() < 0) setVelocityY(0.5);
+        if(getY() < 0) setVelocityY(1);
+
+        // Rotate when dead
+        if(isDead) {
+            setRotate(getRotate() - 1);
+        }
 
     }
 
