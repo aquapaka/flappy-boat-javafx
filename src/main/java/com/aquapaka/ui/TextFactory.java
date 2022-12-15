49dcf3a3bfb0d428353ee2f860a4733fbbd1bb59
aquapaka <aqua.tamlong@gmail.com>
2022-12-15 19:07:45 +0700

    Change text ui color

--- src/main/java/com/aquapaka/ui/TextFactory.java
+++ src/main/java/com/aquapaka/ui/TextFactory.java
@@ -7,11 +7,10 @@ import javafx.scene.text.Text;
 
 public class TextFactory {
     public static final String FONT_PATH = FlappyBoat.class.getResource("/font/VT323-Regular.ttf").toExternalForm();
-    public static final Font GAME_FONT = Font.loadFont(FONT_PATH, 24);
 
     public static Text getText(String textValue, int fontSize) {
         Text text = new Text(textValue);
-        text.setFill(Color.color(1, 0.6, 0.2));
+        text.setFill(Color.color(1, 0.8, 0.6));
         text.setFont(Font.loadFont(FONT_PATH, fontSize));
 
         return text;
